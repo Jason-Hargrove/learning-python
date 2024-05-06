1fe8cd435d4d475a36ad6324aa255088a36c3af5
Jason Hargrove <rijaha@gmail.com>
2024-05-05 22:50:27 -0500

    function with variable number of arguments

--- chapter-2-basics/functions_start.py
+++ chapter-2-basics/functions_start.py
@@ -17,15 +17,32 @@ def cube(x):
     return x * x * x
 
 # TODO: function with default value for an argument
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
 
 # TODO: function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
 
 # func1()
 # print(func1())
 # print(func1)
 
-func2(10, 20)
-print(func2(10, 20))
-print(func2)
-print(cube(3))
+# func2(10, 20)
+# print(func2(10, 20))
+# print(func2)
+# print(cube(3))
+
+# print(power(5))
+# print(power(2,3))
+
+# print(power(x=3, num=2))
+
+print(multi_add(4,5,10,4,10))
