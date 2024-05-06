82363049147bba8358eb5c79e33f3e19e5aa33a8
Jason Hargrove <rijaha@gmail.com>
2024-05-05 22:34:50 -0500

    define basic function

--- chapter-2-basics/functions_start.py
+++ chapter-2-basics/functions_start.py
@@ -5,15 +5,27 @@
 
 
 # TODO: define a basic function
-
+def func1():
+    print("I'm a function")
 
 # TODO: function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # TODO: function that returns a value
-
+def cube(x):
+    return x * x * x
 
 # TODO: function with default value for an argument
 
 
 # TODO: function with variable number of arguments
+
+func1()
+print(func1())
+print(func1)
+
+func2(10, 20)
+print(func2(10, 20))
+print(func2)
+print(cube(3))
