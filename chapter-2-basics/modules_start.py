bd6b7e402c38e662ca4614c6465199cc727fda73
Jason Hargrove <rijaha@gmail.com>
2024-05-06 22:15:54 -0500

    cleaned up spaces

--- chapter-2-basics/modules_start.py
+++ chapter-2-basics/modules_start.py
@@ -1,10 +1,8 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-
 # TODO: import the math module, which contains features for working with mathematics
 import math
-
 
 # TODO: the math module contains lots of pre-built functions
 print("The square root of 16 is",math.sqrt(16))
