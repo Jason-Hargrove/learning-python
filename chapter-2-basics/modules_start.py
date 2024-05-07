7b538aae4ca5d4b424d4dd6b431d54ba5b1c1988
Jason Hargrove <rijaha@gmail.com>
2024-05-06 22:15:32 -0500

    modules

---
+++ chapter-2-basics/modules_start.py
@@ -0,0 +1,26 @@
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+# TODO: import the math module, which contains features for working with mathematics
+import math
+
+
+# TODO: the math module contains lots of pre-built functions
+print("The square root of 16 is",math.sqrt(16))
+
+# TODO: in addition to functions, some modules contain useful constants 
+print("Pie is",math.pi)
+
+# TODO: try some of the math functions for yourself here:
+# math.floor
+number = 3.7
+floor_value = math.floor(number)
+print(floor_value)
+
+negative_number = -3.7
+negative_floor_value = math.floor(negative_number)
+print(negative_floor_value)
+
+celing_value = math.ceil(number)
+print("This is the ceiling value of number:",celing_value)
