90002bedf1bf5b272b333519a362814463889d7d
Jason Hargrove <rijaha@gmail.com>
2024-05-12 22:39:09 -0500

    done timedeltas

--- chapter-4-dates-and-times/timedeltas.py
+++ chapter-4-dates-and-times/timedeltas.py
@@ -31,10 +31,15 @@ s = t.strftime("%A %B %d, %Y")
 print("One week ago it was", s)
 
 ### How many days until April Fools' Day?
-
+today = date.today()
+afd = date(today.year, 4, 1)
 
 # TODO: use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
+if afd < today:
+    print("April Fools' Day already went by:", ((today-afd).days))
+    afd = afd.replace(year = today.year + 1)
 
-
-# TODO: Now calculate the amount of time until April Fool's Day  
+# TODO: Now calculate the amount of time until April Fool's Day 
+time_to_afd = afd - today
+print("It is",time_to_afd.days," days until the next April Fools' Day")
