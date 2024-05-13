10e5e842e8aaa3b1fc5ed9b1ec527c3c5d5b5fde
Jason Hargrove <rijaha@gmail.com>
2024-05-12 22:23:10 -0500

    added timedelta file

---
+++ chapter-4-dates-and-times/timedeltas.py
@@ -0,0 +1,36 @@
+#
+# Example file for working with timedelta objects
+# LinkedIn Learning Python course by Joe Marini
+#
+
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+
+# TODO: construct a basic timedelta and print it
+print(timedelta(days=365, hours=5, minutes=1))
+print(timedelta(days=400, hours=10, minutes=5))
+
+# TODO: print today's date
+
+
+# TODO: print today's date one year from now
+
+
+# TODO: create a timedelta that uses more than one argument
+
+
+# TODO: calculate the date 1 week ago, formatted as a string
+
+
+### How many days until April Fools' Day?
+
+
+# TODO: use date comparison to see if April Fool's has already gone for this year
+# if it has, use the replace() function to get the date for next year
+
+
+# TODO: Now calculate the amount of time until April Fool's Day  
