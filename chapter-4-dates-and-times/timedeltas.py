aa8c95510e25ce5bbbd9e85ebcb8cb86725baccc
Jason Hargrove <rijaha@gmail.com>
2024-05-12 22:32:38 -0500

    timedelta

--- chapter-4-dates-and-times/timedeltas.py
+++ chapter-4-dates-and-times/timedeltas.py
@@ -15,16 +15,20 @@ print(timedelta(days=365, hours=5, minutes=1))
 print(timedelta(days=400, hours=10, minutes=5))
 
 # TODO: print today's date
+now = datetime.now()
+print("Today is",now)
 
 
 # TODO: print today's date one year from now
-
+print("One year from now will be", str(now + timedelta(days=365) ))
 
 # TODO: create a timedelta that uses more than one argument
-
+print("In two weeks and three days it will be", str(now + timedelta(weeks=2, days=3)))
 
 # TODO: calculate the date 1 week ago, formatted as a string
-
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print("One week ago it was", s)
 
 ### How many days until April Fools' Day?
 
