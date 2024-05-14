1b8db8881ea129e5f30333b4396ee25730024a28
Jason Hargrove <rijaha@gmail.com>
2024-05-14 15:53:15 -0500

    finished code challenge

--- chapter-4-dates-and-times/playground.py
+++ chapter-4-dates-and-times/playground.py
@@ -1,7 +1,16 @@
 import calendar
 
 def count_days(year, month, whichday):
-    # Your code goes here.
-    result = 0
-    return result
+    # Generate the calendar for the month
+    cal = calendar.monthcalendar(year, month)
+    day_count = 0
 
+    # Count occurances of the specific day of the week
+    for week in cal:
+        if week[whichday] != 0: # The day of the week is in this week
+            day_count += 1
+
+    return day_count
+
+print(count_days(2025, 12, 0))
+#expected result: 5
