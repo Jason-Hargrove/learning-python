cc2a3655963d899eed9e20a98501defd98e7e4c4
Jason Hargrove <rijaha@gmail.com>
2024-05-15 17:10:12 -0500

    finished fetching internet data

--- chapter-5-internet-data/inetdata.py
+++ chapter-5-internet-data/inetdata.py
@@ -3,8 +3,13 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+import urllib.request
+
 def main():
-    pass # this is a placeholder, do-nothing statement
+    weburl = urllib.request.urlopen("http://www.jasonhargroveart.com/")
+    print("result code:", weburl.getcode()) # 200 if ok, 400 if not found
+    data = weburl.read()
+    print(data)
 
 if __name__ == "__main__":
     main()
